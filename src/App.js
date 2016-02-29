b465bd58ab404f7b13033a507668bc2607ff0dc3
Maurice <mcsmorenberg@gmail.com>
2016-02-29 15:01:53 +0100

    todolist

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         return (
             <div className="todos-app">
               <TodoForm model={this.todos} />
-
+              <TodoList model={this.todos} />
             </div>
         );
     }
